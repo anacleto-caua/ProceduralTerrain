d9fe71a007430b2f153d86db78e652044ebdea08
Cauã Anacleto <anacleto.for.work@gmail.com>
2025-07-15 19:31:57 -0300

    Procedural generation demo

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,31 +1,87 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Chunk : MonoBehaviour
 {
     private const float UNITY_DEFAULT_PLANE_SCALE = 10f;
 
+    private Color defaultDebugColor = Color.cyan;
+
+    private int CHUNK_SIZE = 0;
+    
+    private const float PROCEDURAL_AMPLITUTE = 10f;
+
     public GameObject plane;
     
+    private Vector2Int gridPos;
 
-    private Vector2Int pos;
+    private Vector3 basePos;
 
-    private GameObject redSphere;
+    private int gridDepth = 10;
 
-    private int CHUNK_SIZE = 50;
+    private int spaceBetweenGridDots = 0;
 
     public void CreateChunk(Vector2Int pos, int chunkSize)
     {
-        this.pos = pos;
+        this.gridPos = pos;
         this.CHUNK_SIZE = chunkSize;
+        this.spaceBetweenGridDots = CHUNK_SIZE / (gridDepth - 1);
 
-        this.gameObject.name = "Chunk-" + pos.x + "-" + pos.y;
+        // This calculates the "0 0 position" for the chunk, since its placed by the center
+        this.basePos = this.transform.position;
+        this.basePos.x -= CHUNK_SIZE / 2;
+        this.basePos.z -= CHUNK_SIZE / 2;
+        CreateDebugSphere(basePos);
+
+        this.gameObject.name = "Chunk|" + pos.x + "|" + pos.y + "|";
         this.plane.transform.localScale = Vector3.one * CHUNK_SIZE / UNITY_DEFAULT_PLANE_SCALE;
-        //SpawnRedSphere();
     }
 
     public void GenerateChunk()
     {
+        FastNoiseLite noise = new FastNoiseLite();
+        noise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
 
+        Vector3 debugSpherePos = Vector3.zero;
+        for (int i = 0; i < this.gridDepth; i++) {
+            debugSpherePos.x = this.basePos.x + this.spaceBetweenGridDots * i;
+
+            for (int j = 0; j < this.gridDepth; j++)
+            {
+                debugSpherePos.z = this.basePos.z + this.spaceBetweenGridDots * j;
+                debugSpherePos.y = 
+                    (noise.GetNoise(
+                        (this.gridPos.x * this.gridDepth) + i, 
+                        (this.gridPos.y * this.gridDepth) + j
+                    ) 
+                    + 1) // Noise goes from 1 to -1, so sum one and it wont go "underground"
+                    * PROCEDURAL_AMPLITUTE;
+
+                CreateDebugSphere(debugSpherePos, Color.blue, .5f, "_debug_sphere_" +  i + "_" + j);
+            }
+        }
+    }
+    public void CreateDebugSphere(Vector3 pos)
+    {
+        CreateDebugSphere(pos, Color.cyan);
+    }
+
+    public void CreateDebugSphere(Vector3 pos, Color color, float scale = 1.0f, string name = "debug_sphere")
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.parent = this.gameObject.transform;
+        sphere.name = name;
+        sphere.transform.position = pos;
+        sphere.transform.localScale = Vector3.one * scale;
+
+        Renderer sphereRenderer = sphere.GetComponent<Renderer>();
+        sphereRenderer.material.color = color;
+    }
+
+    void Log()
+    {
+        Debug.Log("Chunk-" + "x: " + gridPos.x + " y: " + gridPos.y);
     }
 
     public void Load()
@@ -33,33 +89,7 @@ public class Chunk : MonoBehaviour
         this.gameObject.SetActive(true);
     }
     public void Unload()
-    {
+     {
         this.gameObject.SetActive(false);
-    }
-
-    public void SpawnRedSphere()
-    {
-        Vector3 spawnPos;
-        spawnPos = new Vector3(pos.x * CHUNK_SIZE, 0, pos.y * CHUNK_SIZE);
-
-        this.redSphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        redSphere.name = "RedSphere";
-        redSphere.transform.position = spawnPos;
-
-        Renderer sphereRenderer = redSphere.GetComponent<Renderer>();
-
-        if (sphereRenderer != null)
-        {
-            sphereRenderer.material.color = Color.red;
-        }
-        else
-        {
-            Debug.LogError("SphereRenderer not found on the newly created sphere GameObject!");
-        }
-    }
-
-    void Log()
-    {
-        Debug.Log("Chunk-" + "x: " + pos.x + " y: " + pos.y);
     }
 }
